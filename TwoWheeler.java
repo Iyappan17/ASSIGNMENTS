43b4ff1105bb75092219a97737e5916d80ae2a3b
Iyappan17 <115522394+Iyappan17@users.noreply.github.com>
2022-10-11 10:22:24 +0530

    Add files via upload
    DAY1

---
+++ TwoWheeler.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class TwoWheeler {
+;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub     
+		int noOfWheels =  2;
+		System.out.println("noOfWheels="+noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println("noOfMirrors="+noOfMirrors);
+		long chassisNumber = 253372286337227862L;
+		System.out.println("chassisNumber="+chassisNumber);
+		boolean isPunctured = false;
+		System.out.println("isPunctured="+isPunctured);
+		String bikeName = "R15";
+		System.out.println("bikeName="+bikeName);
+		double  runningKM =7456.54;
+		System.out.println("runningKM="+runningKM);
+		
+	}
+
+}
---
+++ mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mobileBrandName = "poco";
+System.out.println("mobileBrandName="+mobileBrandName);
+char mobileLogo = 'p';
+System.out.println("mobileLogo="+mobileLogo);
+short noOfPiece = 5634;
+System.out.println("noOfpiece="+noOfPiece);
+int modelNumber = 5687;
+System.out.println("modelNumber="+modelNumber);
+float mobilePrice = 39999.99F;
+System.out.println("mobilePrice="+mobilePrice);
+boolean isDamaged = false;
+System.out.println("isDamaged="+isDamaged);
+	}
+
+}
